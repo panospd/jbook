34fe9f736797ff05650dddf3b90e423cb92fd6ce
Panagiotis Anastasiadis <panos@192.168.1.222>
2021-04-15 16:02:35 +0100

    Implemented redux scaffolding

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
-import CodeCell from './components/code-cell';
+import { Provider } from 'react-redux';
 import TextEditor from './components/text-editor';
+import { store } from './state';
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
