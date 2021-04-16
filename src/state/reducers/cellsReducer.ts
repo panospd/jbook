c3324c51ee300e38ee00aacc79b74a3c2afe9fa5
Panagiotis Anastasiadis <panos@192.168.1.222>
2021-04-16 13:01:04 +0100

    Implemented adding code/text cells after and fixing instant show of buttons at the bottom

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -44,7 +44,7 @@ const reducer = produce(
         state.order[index] = state.order[targetIndex];
         state.order[targetIndex] = identifier;
         return state;
-      case ActionType.INSERT_CELL_BEFORE:
+      case ActionType.INSERT_CELL_AFTER:
         const cell: Cell = {
           id: randomId(),
           content: '',
@@ -58,11 +58,11 @@ const reducer = produce(
         );
 
         if (foundIndex < 0) {
-          state.order.push(cell.id);
+          state.order.unshift(cell.id);
           return state;
         }
 
-        state.order.splice(foundIndex, 0, cell.id);
+        state.order.splice(foundIndex + 1, 0, cell.id);
 
         return state;
       default:
