3bf719c6a26a8a32b1efb3222783704419dd4079
Panos Anastasiadis <anastapd@gmail.com>
2021-04-19 08:59:08 +0100

    created hook to get cumulative code for each cell

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -7,6 +7,7 @@ import { Cell } from '../state';
 import CodeEditor from './code-editor';
 import Preview from './preview';
 import Resizable from './resizable';
+import { useCumulativeCode } from '../hooks/use-cumulative-code';
 
 interface CodeCellProps {
   cell: Cell;
@@ -22,43 +23,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     err: '',
   };
 
-  const cumulativeCode = useTypedSelector(state => {
-    const { data, order } = state.cells;
-    const orderedCells = order.map(id => data[id]);
-
-    const cumulativeCode = [
-      `
-        import _React from 'react'
-        import _ReactDOM from 'react-dom'
-
-        const root = document.querySelector('#root')
-
-        const show = (value) => {
-          if(typeof value === 'object') {
-            if(value.$$typeof && value.props) {
-              _ReactDOM.render(value, root)
-            }else {
-              root.innerHTML =  JSON.stringify(value);
-            }
-          }else {
-            root.innerHTML = value;
-          }
-        }
-      `,
-    ];
-
-    for (let c of orderedCells) {
-      if (c.type === 'code') {
-        cumulativeCode.push(c.content);
-      }
-
-      if (c.id == cell.id) {
-        break;
-      }
-    }
-
-    return cumulativeCode;
-  });
+  const cumulativeCode = useCumulativeCode(cell.id);
 
   useEffect(() => {
     if (!cell.content) {
@@ -66,7 +31,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     }
 
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cumulativeCode.join('\n'));
+      createBundle(cell.id, cumulativeCode);
     }, 2000);
 
     return () => {
